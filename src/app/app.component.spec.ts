9535d0640267d6dd341c209b3ddcb969fb4fea04
bmVsc29u <bmVsc29u@gmail.com>
2019-05-09 23:15:13 +0800

    test(demo): fix specs

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,18 +1,33 @@
-import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { RouterTestingModule } from '@angular/router/testing';
 
-describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  }));
+import { Spec } from '@chakray/utils/testing';
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClient } from '@angular/common/http';
+import { of } from 'rxjs';
+const mockHttp = {
+  get() { return of('# test\ncontent\na'); }
+};
+
+import { AppComponent as Tag } from './app.component';
+
+Spec.tag(Tag, {
+  schemas: [NO_ERRORS_SCHEMA],
+  imports: [
+    RouterTestingModule,
+    HttpClientTestingModule
+  ],
+  providers: [
+    { provide: HttpClient, useValue: mockHttp }
+  ]
+}, (ref) => {
+  let t: Tag;
+  beforeEach(() => {
+    t = ref.tag;
+  });
+
+  it('should handle http.get', () => {
+    expect(t.data.setup).toEqual({ title: 'test', content: 'content\na' });
   });
 });
